71ad37d43d80fcb4e195eedc79282d7fa7d0057a
刘家卫 <1014168650@qq.com>
2019-05-15 17:53:22 +0800

    继续提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @version 1.0
+ * @user 刘家卫
+ * @date 2019/5/15 17:12
+ */
+public class Main {
+     public static void main(String[] args) {
+          System.out.println("hello git");
+     }
+}
